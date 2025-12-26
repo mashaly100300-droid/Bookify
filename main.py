9c257c4e9eef21aedb5176f40dea0e736cc7bdda
mashaly100300-droid <mashaly100300@gmail.com>
2025-12-26 14:20:46 +0200

    my first update

--- main.py
+++ main.py
@@ -42,3 +42,6 @@ app.include_router(cetegory_route.router)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+
+#######
--- statics.py
+++ statics.py
@@ -2,3 +2,6 @@ import sqlite3
 
 class statics :
      DATABASE_NAME = "D:\\Programing\\programing1\\python\\bookify\\bookify.db"
+
+
+#####
